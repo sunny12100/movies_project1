16f9bb967a48725be2034f2e818b0af1f8bedcac
sunny12100 <sunnygupta123420@gmail.com>
2024-08-13 10:36:28 +0530

    Login css completed

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute w-full bg-gradient-to-b from-black'>
+    <div className='absolute w-full bg-gradient-to-b from-black z-10'>
       <img className='h-25 w-40 mx-20 py-6 ' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="logo" />
     </div>
   )
--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [SignInForm , setSignInForm] = useState(true);
+  const toggleSignInForm =()=>{
+    setSignInForm(!SignInForm);
+  }
+
   return (
-    <div >
+    <div>
       <Header/>
-      <div className='absolute'>
+      <div className='absolute z-5'>
       <img src="https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/19fc1a4c-82db-4481-ad08-3a1dffbb8c39/IN-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_24a485f6-1820-42be-9b60-1b066f1eb869_large.jpg" alt="" />
       </div>
-      <form className='relative flex flex-col text-white'>
-        <h1>Sign In</h1>
-        <input type="text" placeholder='Email Address'/>
-        <input type="text" placeholder='Password'/>
-        <button>Sign In</button>
-        <p>New to Netflix? Sign Up Now!</p>
+      <form className='absolute flex flex-col rounded-md opacity-100 text-white bg-[rgba(0,0,0,0.7)] w-1/3 mx-auto right-0 left-0 h-3/4 my-28'>
+        <h1 className='text-[2rem] font-bold mx-20 mt-8'>{SignInForm? 'Sign In' : 'Sign Up'}</h1>
+        {!SignInForm && (<input className='min-h-12 min-w-4 mx-20 mt-4 bg-transparent text-white border-white rounded-md' type="text" placeholder='Full Name'/>)}
+        <input className='min-h-12 min-w-4 mx-20 mt-4 bg-transparent text-white border-white rounded-md' type="text" placeholder='Email Address'/>
+        <input className='min-h-12 min-w-4 mx-20 mt-6 bg-transparent text-white border-white rounded-md' type="text" placeholder='Password'/>
+        <button className='mt-10 mx-20 rounded-md text-lg bg-red-600 min-w-4 min-h-12'>Sign In</button>
+        <p className='mt-10 text-white opacity-50 text-2xl mx-auto'>OR</p>
+        <p className='mx-20 text-xl mt-8 text-white cursor-pointer hover:underline ' onClick={toggleSignInForm}>{SignInForm?"New to Netflix? Sign Up Now!":"Already a User? Sign In Now!"}</p>
       </form>
     </div>
   )
